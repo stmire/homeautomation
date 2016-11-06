ed4a4754d48d64dddc7cb0d9d374bbb7f045d4fc
stmire <sreilly826@gmail.com>
2016-11-06 17:09:30 -0500

    Add comments

--- backup.py
+++ backup.py
@@ -1,51 +1,53 @@
 '''
-Home Backup
+Directory Backup
 
-This will be used to backup a home folder
-for a specified user. The results of which will
-be moved into a ZFS mountpoint
+Run inside any directory to zip the contents of that current working directory.
+Once zipped, this will move them into a ZFS mountpoint which is mapped to a
+dataset on an external drive.
 '''
 
 import zipfile
 import shutil
 import os
+import datetime
 
-'''
+# Directory to be backed up
+source = os.getcwd()
+
+# Absolute path of current working directory
+absolutePath = os.path.abspath(os.getcwd())
+
+# ZFS mountpoint to store zip file
+destination = '/mypool/backup'
+
+# Current date and time
+now = str(datetime.datetime.now().strftime('%m-%d-%Y-%H-%M-%S'))
+
+# Zip file name
+zipname = source + '-' + now + '.zip'
+
+# Create zip file object in write mode
+zipper = zipfile.ZipFile(zipname, 'w', allowZip64 = True)
+
+# Walk through each file in the current working directory and write to zip file
 def backup():
-    os.chdir('/home/')
-    folder = os.path.abspath(user)
-    print('Creating zip file...')
-    for root, dirs, files in os.walk(folder):
-        for filename in files:
-            zippy.write(os.path.join(root, filename))
-    zippy.close()
-    move()
-
-def move():
-    os.chdir('/home/steve/homeautomation')
-    shutil.move(zipname, '/mypool/backup')
+    print('Creating ' + zipname + ' file...')
+    for files in os.listdir(source):
+        zipper.write(files)
+    zipper.close()
     print('Done.')
 
-user = raw_input('Backup which user: ')
-zipname = user+'.zip'
-zippy = zipfile.ZipFile(zipname, 'w', allowZip64 = True)
-
-backup()
-'''
-
-def backup():
-    cwd = os.getcwd()
-    print('Creating zip file...')
-    for root, dirs, files in cwd:
-        for filename in files:
-            zippy.write(os.path.join(root, filename))
-    zippy.close()
-    move()
-
+# Move zip file to ZFS mountpoint
 def move():
-    os.chdir('/home/steve/homeautomation')
-    shutil.move(zipname, '/mypool/backup')
+    print('Moving to ZFS mountpoint')
+    os.chdir(absolutePath)
+    shutil.move(zipname, destination)
     print('Done.')
 
-zipname = os.getcwd()+'.zip'
-zippy = zipfile.ZipFile(zipname, 'w', allowZip64 = True)
+#Main function
+def main():
+    backup()
+    move()
+
+if __name__ == '__main__':
+    main()
