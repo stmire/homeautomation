ee73a390a836e42242a71203353abef643fd0e75
stmire <sreilly826@gmail.com>
2016-11-06 16:47:25 -0500

    Complete rewrite

--- backup.py
+++ backup.py
@@ -10,7 +10,8 @@ import zipfile
 import shutil
 import os
 
-def backup(user):
+'''
+def backup():
     os.chdir('/home/')
     folder = os.path.abspath(user)
     print('Creating zip file...')
@@ -29,4 +30,22 @@ user = raw_input('Backup which user: ')
 zipname = user+'.zip'
 zippy = zipfile.ZipFile(zipname, 'w', allowZip64 = True)
 
-backup(user)
+backup()
+'''
+
+def backup():
+    cwd = os.getcwd()
+    print('Creating zip file...')
+    for root, dirs, files in cwd:
+        for filename in files:
+            zippy.write(os.path.join(root, filename))
+    zippy.close()
+    move()
+
+def move():
+    os.chdir('/home/steve/homeautomation')
+    shutil.move(zipname, '/mypool/backup')
+    print('Done.')
+
+zipname = os.getcwd()+'.zip'
+zippy = zipfile.ZipFile(zipname, 'w', allowZip64 = True)
